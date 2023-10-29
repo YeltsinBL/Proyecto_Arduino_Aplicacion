a6bbb84140d5ef6325765abb6ab547b3b1f5c428
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2023-10-29 01:18:09 -0500

    Modificación Tiempo y Humedad
    Se agregó un tiempo de envío para los valores de la humedad y ḿodificó
    la funcionalidad de los botones para no mezclar valores del sistema y
    protoboard

--- codigo_v1/codigo_v1.ino
+++ codigo_v1/codigo_v1.ino
@@ -4,13 +4,15 @@ Servo myservo;
 
 int pinservo=9;
 int angulo = 90;
+int carril=0;
+
+/**botones*/
 int pulsador1=6;
 int led1=11;
 int pulsador2=7;
 int led2=10;
 int estado_pulsador1=0;
 int estado_pulsador2=0;
-int carril=0;
 
 /**sensor de humedad*/
 const int Airvalue = 472;
@@ -19,9 +21,11 @@ int soilMoistureValue = 0;
 int soilmoisturepercent=0;
 
 /**Controlar el inicio y apagado del sistema*/
-bool stringComplete=true;
+//bool stringComplete=true;
 String inputString ="apagado";
 int iniciar=0;
+const long interval = 2500; // tiempo a enviar
+unsigned long previousMillis = 0;  // Tiempo anterior 0
 
 void setup() {
   Serial.begin(9600);
@@ -29,13 +33,17 @@ void setup() {
 
   pinMode(led1,INPUT);
   pinMode(led2,INPUT);
+
   myservo.attach(pinservo);
+  previousMillis = millis(); 
 }
 
 void loop() {
   //Serial.println("stringComplete:" + stringComplete);
-  if(stringComplete){
-    //Serial.println("inputString:" + inputString);
+  //if(stringComplete){
+    // if (millis() - previousMillis >= interval) {
+       //Serial.println("inputString:" + inputString.substring(0,1));
+    // }
     if(inputString.substring(0,1)=="a"){
       //Serial.println("Apagado");
       digitalWrite(led1, LOW);
@@ -45,9 +53,17 @@ void loop() {
     }
     if(inputString.substring(0,1)=="e"){
       //Serial.println("Encendido");
-      botones();
+      //botones();
+      // digitalWrite(led2, HIGH);
+      // digitalWrite(led1, LOW);
+      angulo=0;
+      inputString = "i";      
       myservo.write(angulo); 
-      sensorHumerdad();
+      // if (millis() - previousMillis >= interval) {
+      //   Serial.println("Entro E");
+      //   previousMillis = millis();
+      //   sensorHumerdad();
+      // }
     }
     if(inputString.substring(0,1)=="i"){
       digitalWrite(led2, HIGH);
@@ -55,8 +71,12 @@ void loop() {
       angulo=0;
       carril=0;
       myservo.write(angulo); 
-      sensorHumerdad();
-      botones();
+      if (millis() - previousMillis >= interval) {
+        //Serial.println("Entro I");
+        previousMillis = millis();
+        sensorHumerdad();
+      }
+      //botones();
     }
     if(inputString.substring(0,1)=="d"){
       digitalWrite(led1, HIGH);
@@ -64,39 +84,32 @@ void loop() {
       angulo=180;
       carril=1;
       myservo.write(angulo); 
-      sensorHumerdad();
-      botones();
+      if (millis() - previousMillis >= interval) { 
+        //Serial.println("Entro D");
+        previousMillis = millis();   
+        sensorHumerdad();
+      }
+      //botones();
     }
-  }
-  delay (1000) ;
+    
+      botones();
+  //}
+  //delay (1000) ;
 }
 void sensorHumerdad(){
-  soilMoistureValue = analogRead (A0) ;
-  Serial.println (soilMoistureValue) ;
+  soilMoistureValue = analogRead (A0);
   soilmoisturepercent = map (soilMoistureValue, Airvalue, WaterValue, 0, 100);
-  // if (soilmoisturepercent < 40) // change this at what level the pump turns on
-  // {
-  // Serial.println ("seco, Pump turning on");
+   if (soilmoisturepercent >= 100){
+   Serial.println("100");
 
-  // }
-  // else if (soilmoisturepercent > 50) // max water level should be
-  // {
-  // Serial.println("mojado, Pump turning off");
-
-  // }
-  //  if (soilmoisturepercent >= 100){
-  //  Serial.println("100");
-
-  //  }
-  //  else if (soilmoisturepercent <=0){
-  //  Serial.println("0");
-
-  //  }
-  //  else if (soilmoisturepercent >0 && soilmoisturepercent < 100){
-  //  Serial.println(soilmoisturepercent);
-  // Serial.println("%");
-  // }
-  delay (1000) ;
+   }
+   else if (soilmoisturepercent <=0){
+   Serial.println("0");
+   }
+   else if (soilmoisturepercent >0 && soilmoisturepercent < 100){
+   Serial.println(soilmoisturepercent);
+  }
+  //delay (1000) ;
 }
 void botones(){
   estado_pulsador1=digitalRead(pulsador1);
@@ -108,6 +121,12 @@ void botones(){
     digitalWrite(led1, HIGH);
     digitalWrite(led2, LOW);
     inputString = "d";
+    myservo.write(angulo);
+    if (millis() - previousMillis >= interval) {
+      //Serial.println("Entro Boton");
+      previousMillis = millis();
+      sensorHumerdad();
+    }
   }
   if(estado_pulsador2==HIGH){
     Serial.println("i");
@@ -116,21 +135,33 @@ void botones(){
     digitalWrite(led2, HIGH);
     digitalWrite(led1, LOW);
     inputString = "i";
+    myservo.write(angulo);
+    if (millis() - previousMillis >= interval) {
+      //Serial.println("Entro Boton");
+      previousMillis = millis();
+      sensorHumerdad();
+    }
   }
-  if(carril==0){
-    digitalWrite(led2, HIGH);
-    digitalWrite(led1, LOW);
-    angulo=0;
-    //Serial.println("Izquierda");
-  }
+  // else
+  // if(carril==0){
+  //   digitalWrite(led2, HIGH);
+  //   digitalWrite(led1, LOW);
+  //   angulo=0;
+  //   inputString = "i";
+  // }
   // else{
   //   digitalWrite(led1, HIGH);
   //   digitalWrite(led2, LOW);
   //   //angulo=180;
   //   //Serial.println("Derecha");
   // }
-  myservo.write(angulo); 
-  sensorHumerdad();
+  // myservo.write(angulo); 
+  
+  // if (millis() - previousMillis >= interval) { 
+  //   Serial.println("Entro Boton");
+  //   previousMillis = millis();   
+  //   sensorHumerdad();
+  // }
 }
 void serialEvent() {
  // Verificar una a una el caractrer de los datos ingresados
